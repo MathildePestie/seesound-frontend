0e6ec6d42d76e31969313b1f3928b885c979bb60
Mathilde Pestie <mathilde.pestie@gmail.com>
2025-04-11 09:54:23 +0200

    mobil block

--- components/DeviceCheck.js
+++ components/DeviceCheck.js
@@ -1,32 +1,62 @@
-import React, { useEffect, useState } from 'react';
+import React, { useEffect, useState } from "react";
+import styles from "../styles/Card.module.css";
 
 const DeviceCheck = () => {
   const [isMobile, setIsMobile] = useState(false);
 
   useEffect(() => {
-    // Vérifie la largeur de l'écran à l'ouverture de la page
     const checkDevice = () => {
-      if (window.innerWidth <= 768) { // On définit ici une largeur pour les mobiles (ex : 768px)
+      if (window.innerWidth <= 768) {
         setIsMobile(true);
       } else {
         setIsMobile(false);
       }
     };
 
-    checkDevice(); // Vérifie au chargement
-    window.addEventListener('resize', checkDevice); // Réajuste lors du redimensionnement de la fenêtre
+    checkDevice();
+    window.addEventListener("resize", checkDevice);
 
     return () => {
-      window.removeEventListener('resize', checkDevice); // Nettoyage du listener
+      window.removeEventListener("resize", checkDevice);
     };
   }, []);
 
   if (isMobile) {
-    // Affiche un message ou fais une redirection si l'utilisateur est sur mobile
     return (
-      <div style={{ textAlign: 'center', padding: '20px', backgroundColor: '#ffcccb' }}>
-        <h2>Le site n'est pas accessible sur mobile</h2>
-        <p>Veuillez accéder au site sur un ordinateur pour une meilleure expérience.</p>
+      <div
+        style={{
+          position: "fixed",
+          top: 0,
+          left: 0,
+          width: "100%",
+          height: "100%",
+          backgroundColor: "black", // un fond semi-transparent
+          display: "flex",
+          justifyContent: "center",
+          alignItems: "center",
+          zIndex: 9999, // Assure-toi que le message reste au-dessus de tout le contenu
+          flexDirection: "column",
+          color: "white",
+          fontSize: "1.5rem",
+          textAlign: "center",
+          fontFamily: "Arial, sans-serif",
+        }}
+      >
+        <h2 style={{ fontSize: "2rem" }}>
+          Le site n'est pas accessible sur mobile
+        </h2>
+        <p>
+          Veuillez accéder au site sur un ordinateur pour une meilleure
+          expérience.
+        </p>
+        <div className={styles.musicIcon}>
+          <svg viewBox="0 0 110 135">
+            <path
+              fill="currentColor"
+              d="M42.9,69.9c3.2,0,6.2.9,8.8,2.4V19.8c0-5.6,8.5-5.6,8.5,0v1.5c2.7,7.1,7.7,11.4,12.2,16.1,15.3,16.1,18.7,28.1-5.4,36.9,8.7-10.6,12.6-19.3-.7-33.4-2-2.1-4.1-4.3-6.1-6.8v52.9c0,15.3-18.6,23-29.4,12.2-10.8-10.9-3.1-29.5,12.2-29.5h0Z"
+            />
+          </svg>
+        </div>
       </div>
     );
   }
